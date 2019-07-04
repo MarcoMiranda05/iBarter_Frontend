3da32b54f9f96b1a8a39db7f03dfa2ce34b4b6c6
MarcoMiranda05 <marmirand@hotmail.com>
2019-07-04 10:43:53 +0100

    logo and navbar on

---
+++ ibarter/src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <img src="https://i.imgur.com/YIAIc4Q.png" alt="logo" className="logo" />
+    );
+  }
+}
+
+export default NavBar;
