875f8771b4ca76804c17b7866c72b45571f3f53a
aidenjl193 <aidenjl193@gmail.com>
2019-07-04 15:23:20 +0100

    stuff

---
+++ ibarter/src/containers/ItemCardContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ItemCardContainer extends React.Component {
+
+
+	render() {
+		return (
+			<div id="item-card-container"></div>
+		)
+	}
+}
+
+export default ItemCardContainer;
