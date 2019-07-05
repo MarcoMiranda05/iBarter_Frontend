172330ca30669c6d6c7d2123e0bd745d9437a610
MarcoMiranda05 <marmirand@hotmail.com>
2019-07-05 11:22:12 +0100

    footer with anchor

--- ibarter/src/components/Footer.js
+++ ibarter/src/components/Footer.js
@@ -4,26 +4,35 @@ const Footer = props => {
   return (
     <footer className="footer">
       <div>
-        <img
-          src="https://i.imgur.com/Zfv1lbP.png"
-          alt="facebook-logo"
-          className="logo"
-        />
-        <img
-          src="https://i.imgur.com/8SnQvrT.png"
-          alt="instagram-logo"
-          className="logo"
-        />
-        <img
-          src="https://i.imgur.com/rOlcOjR.png"
-          alt="twitter-logo"
-          className="logo"
-        />
-        <img
-          src="https://i.imgur.com/p7IdfXS.png"
-          alt="youtube-logo"
-          className="logo"
-        />
+        <a href="#">
+          <img
+            href="www.facebook.com"
+            src="https://i.imgur.com/Zfv1lbP.png"
+            alt="facebook-logo"
+            className="logo"
+          />
+        </a>
+        <a href="#">
+          <img
+            src="https://i.imgur.com/8SnQvrT.png"
+            alt="instagram-logo"
+            className="logo"
+          />
+        </a>
+        <a href="#">
+          <img
+            src="https://i.imgur.com/rOlcOjR.png"
+            alt="twitter-logo"
+            className="logo"
+          />
+        </a>
+        <a href="#">
+          <img
+            src="https://i.imgur.com/p7IdfXS.png"
+            alt="youtube-logo"
+            className="logo"
+          />
+        </a>
       </div>
       <p className="copyright">
         &copy; 2019 iBarter - Aiden Leeming & Marco Miranda - Flatiron School
